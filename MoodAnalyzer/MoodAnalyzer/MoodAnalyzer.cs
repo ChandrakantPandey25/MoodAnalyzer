d1ea537fe4bad3ad5ad3e6915c5af7bec9991b2f
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-09-04 16:57:32 -0700

    Added Ability to analyse and respond Happy and Sad Mood

---
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyzer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzerTestCases
+{
+    public class MoodAnalyzer
+    {
+        public string Message;
+        public MoodAnalyzer()
+        {
+
+        }
+        public MoodAnalyzer(string Message)
+        {
+            this.Message = Message.ToUpper();
+        }
+
+        public string AnalyseMoodWithconstructor(string Message)
+        {
+            if (Message.ToLower().Contains("sad"))
+                return "SAD";
+            else return "HAPPY";
+        }
+        public string AnalyseMoodWithparameterizedconstructor()
+        {
+            if (Message.ToLower().Contains("sad"))
+                return "SAD";
+            else return "HAPPY";
+        }
+
+
+
+    }
+}
---
+++ MoodAnalyzer/MoodAnalyzerTestCases/UnitTest1.cs
@@ -0,0 +1,47 @@
+using NUnit.Framework;
+
+
+namespace MoodAnalyzerTestCases
+{
+    public class Tests
+    {
+        MoodAnalyzer moodAnalyser;
+        [SetUp]
+        public void Setup()
+        {
+            moodAnalyser = new MoodAnalyzer();
+        }
+
+        [Test]
+        public void GivenMessage_WhenSad_ShouldReturnSadDefaultConstructor()
+        {
+            moodAnalyser = new MoodAnalyzer();
+            string message = "I am in Sad mood";
+            string result = moodAnalyser.AnalyseMoodWithconstructor(message);
+            Assert.AreEqual("SAD",result);
+        }
+        [Test]
+        public void GivenMessage_WhenSad_ShouldReturnHappyDefaultConstructor()
+        {
+            moodAnalyser = new MoodAnalyzer();
+            string message = "I am in Any mood";
+            string result = moodAnalyser.AnalyseMoodWithconstructor(message);
+            Assert.AreEqual("HAPPY", result);
+        }
+        [Test]
+        public void GivenMessage_WhenSad_ShouldReturnSadParameterizedConstructor()
+        {
+            moodAnalyser = new MoodAnalyzer("I am in SAD Mood");            
+            string message = moodAnalyser.AnalyseMoodWithparameterizedconstructor();
+            Assert.AreEqual("SAD", message);
+        }
+        [Test]
+        public void GivenMessage_WhenHappy_ShouldReturnHappyParameterizedConstructor()
+        {
+            moodAnalyser = new MoodAnalyzer("I am in Happy Mood");           
+            string message = moodAnalyser.AnalyseMoodWithparameterizedconstructor();
+            Assert.AreEqual("HAPPY", message);
+        }
+
+    }
+}
